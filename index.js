0dbd926e88c9eaf0d67b6946a382931a425c924b
joshhigg <136753578+joshhigg@users.noreply.github.com>
2023-10-20 14:05:04 -0600

    added questions

--- index.js
+++ index.js
@@ -1,13 +1,48 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const fs = require('fs');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Project Title:'
+    }, 
+    {
+        type: 'input',
+        name: 'desc',
+        message: 'Project description:',
+    },
+    {
+        type: 'input',
+        name: 'install',
+        message: 'Installation instructions:',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Usage Information:',
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'Contribution Guidlines:',
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'Test instructions:',
+    }];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
+
+
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+}
 
 // Function call to initialize app
 init();
